e23bda8cf0d9d994fabef3752c52eaa204af80ed
vano144 <justworkkit@gmail.com>
2017-06-04 16:25:20 +0300

    Fix init script

--- init.sh
+++ init.sh
@@ -6,11 +6,13 @@ then
 		sudo apt-get -y install python-pip
 		sudo pip install virtualenv
 		virtualenv -p python3 env
+		sudo apt-get install python-opencv
 	fi
 
 	if [ $uname == "Darwin" ];
 	then
 		python3 -m venv env
+		env/bin/pip3 install opencv-python
 	fi
 
 	env/bin/pip install -r requirements.txt
@@ -18,6 +20,7 @@ elif [ "$1." == "deploy." ];
 	then
 	sudo apt-get -y install python3, python, python-pip
 	pip install -r requirements.txt
+	sudo apt-get install python-opencv
 else
 	env/bin/python manage.py $*
 fi
